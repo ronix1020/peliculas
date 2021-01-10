0eaa162ea3bd3417e06413b0a1537fa44a0a545d
ronix1020 <ronix1020@users.noreply.github.com>
2021-01-10 10:38:24 -0600

    Inicio aplicacion peliculas

---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:peliculas/src/widgets/card_swiper_widget.dart';
+
+class HomePage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: false,
+        title: Text('Peliculas en cines'),
+        backgroundColor: Colors.indigoAccent,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: (){},
+          )
+        ],
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            _listaPeliculas(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _listaPeliculas() {
+    return CardSwiper(
+      peliculas: [1,2,3,4,5],
+      );
+  }
+}
