59e6ba6cae6b91309f2cbc2e119ee483b5c07044
Rony <ronyalexis77@gmail.com>
2021-01-11 23:12:53 -0600

    PageView del peliculas populares

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:peliculas/src/providers/peliculas_provider.dart';
 import 'package:peliculas/src/widgets/card_swiper_widget.dart';
+import 'package:peliculas/src/widgets/movie_horizontal.dart';
+
+import '../widgets/card_swiper_widget.dart';
 
 class HomePage extends StatelessWidget {
-
   final peliculasProvider = new PeliculasProvider();
 
   @override
@@ -16,14 +18,16 @@ class HomePage extends StatelessWidget {
         actions: [
           IconButton(
             icon: Icon(Icons.search),
-            onPressed: (){},
+            onPressed: () {},
           )
         ],
       ),
       body: Container(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
             _listaPeliculas(),
+            _footer(context),
           ],
         ),
       ),
@@ -31,28 +35,45 @@ class HomePage extends StatelessWidget {
   }
 
   Widget _listaPeliculas() {
-
     return FutureBuilder(
       future: peliculasProvider.getEnCines(),
       builder: (BuildContext context, AsyncSnapshot<List> snapshot) {
-
-        if (snapshot.hasData ){
-          return CardSwiper( peliculas: snapshot.data );
+        if (snapshot.hasData) {
+          return CardSwiper(peliculas: snapshot.data);
         } else {
           return Container(
-            height: 400.0,
-            child: Center(
-              child: CircularProgressIndicator()
-              )
-              );
+              height: 400.0, child: Center(child: CircularProgressIndicator()));
         }
       },
     );
+  }
 
-    // peliculasProvider.getEnCines(); 
-
-    // return CardSwiper(
-    //   peliculas: [1,2,3,4,5],
-    //   );
+  Widget _footer(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: EdgeInsets.only(left: 20.0),
+            child: Text(
+              'Populares',
+              style: Theme.of(context).textTheme.subhead,
+            ),
+          ),
+          SizedBox(height: 5.0,),
+          FutureBuilder(
+            future: peliculasProvider.getPopulares(),
+            builder: (BuildContext context, AsyncSnapshot<List> snapshot) {
+              if (snapshot.hasData) {
+                return MovieHorizontal(peliculas: snapshot.data);
+              } else {
+                return Center(child: CircularProgressIndicator());
+              }
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/src/providers/peliculas_provider.dart
+++ lib/src/providers/peliculas_provider.dart
@@ -2,30 +2,31 @@ import 'package:peliculas/src/models/pelicula_model.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-
-
 class PeliculasProvider {
-
   String _apiKey = 'facb0c43f66a958cbe125ae9e085cd01';
   String _url = 'api.themoviedb.org';
   String _language = 'es-ES';
 
-
-  Future<List<Pelicula>> getEnCines() async {
-
-    final url = Uri.https(_url, '3/movie/now_playing', {
-      'api_key'   : _apiKey,
-      'language'  : _language
-    });
-
-    final res = await http.get( url );
-    final decodedData = json.decode( res.body );
+  Future<List<Pelicula>> _procesarRespuesta(Uri url) async {
+    final res = await http.get(url);
+    final decodedData = json.decode(res.body);
 
     final peliculas = new Peliculas.fromJsonlist(decodedData['results']);
 
     return peliculas.items;
-
   }
 
+  Future<List<Pelicula>> getEnCines() async {
+    final url = Uri.https(_url, '3/movie/now_playing',
+        {'api_key': _apiKey, 'language': _language});
 
+    return await _procesarRespuesta(url);
+  }
+
+  Future<List<Pelicula>> getPopulares() async {
+    final url = Uri.https(
+        _url, '3/movie/popular', {'api_key': _apiKey, 'language': _language});
+
+    return await _procesarRespuesta(url);
+  }
 }
